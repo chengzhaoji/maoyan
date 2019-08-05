07c202804dd91592d39795e7342ad477eff69378
chengzhaoji <2537490611@qq.com>
2019-08-05 16:09:45 +0800

    引进axios

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+import MintUI from "mint-ui"
 
+import "mint-ui/lib/style.css"
+
+
+Vue.prototype.axios=axios
+Vue.use(MintUI)
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
